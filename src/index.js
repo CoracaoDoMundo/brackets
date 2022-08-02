e86f75c4731c4f3fbf951fa558f11ddd8fce6261
CoracaoDoMundo <lazakovich@gmail.com>
2022-08-03 00:44:05 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const openBrackets = bracketsConfig.flat().filter((_, i) => i % 2 == 0);
+    const closeBrackets = bracketsConfig.flat().filter((_, i) => i % 2 !== 0);
+
+    let bracketsComplects = {};
+
+    for (let i = 0; openBrackets.length > i; i++) {
+      bracketsComplects[closeBrackets[i]] = openBrackets[i];
+    }
+
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      let currentBracket = str[i];
+
+      let topBracket = stack[stack.length - 1];
+
+      if (
+        bracketsComplects[currentBracket] === topBracket &&
+        stack.length > 0
+      ) {
+        stack.pop();
+      } else {
+        stack.push(currentBracket);
+      }
+    }
+    return stack.length === 0;
+  };
+
